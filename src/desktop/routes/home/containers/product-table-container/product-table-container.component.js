feab3c2b6238dbe2b4c833aea2c0ce7410b03ee5
Justin Pumford <justin.pumford@domo.com>
2016-05-26 17:17:32 -0600

    feat: make filterable with checkbox

--- src/desktop/routes/home/containers/product-table-container/product-table-container.component.js
+++ src/desktop/routes/home/containers/product-table-container/product-table-container.component.js
@@ -12,20 +12,52 @@ module.exports = ngModule => {
 
   function productTableContainerCtrl(productsProvider) {
     const ctrl = this;
+    // private
+    let _hideOutOfStock = false;
+    let _fullProducts = [];
 
+    // public
     ctrl.$onInit = $onInit;
+    ctrl.onCheckboxUpdate = onCheckboxUpdate;
 
-    ctrl.products = [];
+    ctrl.filteredProducts = [];
     ctrl.loading = true;
 
     function $onInit() {
       // Called on each controller after all the controllers have been constructed and had their bindings initialized
       // Use this for initialization code.
+      refreshProducts();
+    }
+
+    function onCheckboxUpdate(newValue) {
+      _hideOutOfStock = newValue;
+      // if our products are loaded, apply the filter
+      if (!ctrl.loading) {
+        ctrl.filteredProducts = filterProducts(_fullProducts, _hideOutOfStock);
+      }
+    }
+
+    // refreshes the stored product list from the provider
+    // warning, mutates this objects state!
+    function refreshProducts() {
       productsProvider.getProducts().then(data => {
-        ctrl.products = data;
+        _fullProducts = data;
+        ctrl.filteredProducts = filterProducts(_fullProducts, _hideOutOfStock);
         ctrl.loading = false;
       }, error => {
         console.log(error);
+      });
+    }
+
+    // filter the products based on whether or not they are out of stock
+    function filterProducts(products, hideOutOfStock) {
+      // bypass filter if we are not supposed to filter
+      if (!hideOutOfStock) {
+        return products;
+      }
+      // else, return a filtered array
+      return products.filter(product => {
+        return product.inStock;
       });
     }
   }
