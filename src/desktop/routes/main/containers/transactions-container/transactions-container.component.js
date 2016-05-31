ad68c721e39f7e5e879fc16a68bacde64bccae3f
Justin Pumford <justin.pumford@domo.com>
2016-05-31 17:41:59 -0600

    fix(transactions-container): comment out service call while waiting for larger bugfix

--- src/desktop/routes/main/containers/transactions-container/transactions-container.component.js
+++ src/desktop/routes/main/containers/transactions-container/transactions-container.component.js
@@ -22,13 +22,14 @@ module.exports = ngModule => {
     function $onInit() {
       // Called on each controller after all the controllers have been constructed and had their bindings initialized
       // Use this for initialization code.
-      $q.all([transactionsAnalyticsService.getTotals(),
-        transactionsAnalyticsService.getTopSellingItem()]).then(data => {
+      $q.all([transactionsAnalyticsService.getTotals()/*,
+        transactionsAnalyticsService.getTopSellingItem()*/]).then(data => {
           ctrl.loading = false;
           ctrl.transactionCount = data[0].transactionCount;
           ctrl.productsSold = data[0].productsSold;
           ctrl.income = data[0].income;
         }, error => {
+          console.log('error retrieving db results!');
           console.log(error);
         });
       //transactionsAnalyticsService.getTopGrossingItem();
