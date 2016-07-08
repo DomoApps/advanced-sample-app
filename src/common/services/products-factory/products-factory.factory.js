c90f9f31ca3c96875f6561c4df5be2ac93ba4128
Justin Pumford <justin.pumford@domo.com>
2016-07-08 14:52:13 -0600

    docs(factories): add documentation illustrating the use of domo.get

--- src/common/services/products-factory/products-factory.factory.js
+++ src/common/services/products-factory/products-factory.factory.js
@@ -1,3 +1,22 @@
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !!!!!!   Note about datasources !!!!!!!
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//
+// Normally in a production app one would
+// gather data from a 'datasource'. This
+// data is gathered through the Domo client
+// included by adding `domo = require('ryuu.js');`
+// to the top of your file.
+//
+// For our purposes we have left sample code
+// using the Domo client to query data.
+// This code is dispersed in comments or at
+// the bottom of relevant methods.
+//
+// More information on the Domo client can
+// be found at https://developer.domo.com
+
+
 // const Query = require('@domoinc/query');
 // const domo = require('ryuu.js');
 
