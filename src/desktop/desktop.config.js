0d5cdabfb5c61b1c07ca9aa0b00989a8ce572ab0
Justin Pumford <justin.pumford@domo.com>
2016-07-15 16:55:27 -0600

    refactor(desktop.config.js): make bracket styles consistent throughout file

--- src/desktop/desktop.config.js
+++ src/desktop/desktop.config.js
@@ -2,28 +2,25 @@ module.exports = config;
 
 function config($urlRouterProvider, $mdThemingProvider, $mdIconProvider) {
   $urlRouterProvider.otherwise('/');
-  const customPrimary =
-    {
-      '50': '#ffffff', // white background color for toolbar
-      '500': '#72b0d7',   // blue color of the label and choices in select
-      '600': '#72b0d7',   // blue color of the selected choice in select
-      '700': '#80c25d',   // green color of little in-stock indicator
-    };
+  const customPrimary = {
+    '50': '#ffffff', // white background color for toolbar
+    '500': '#72b0d7',   // blue color of the label and choices in select
+    '600': '#72b0d7',   // blue color of the selected choice in select
+    '700': '#80c25d',   // green color of little in-stock indicator
+  };
   const customPrimaryPalette = $mdThemingProvider.extendPalette('blue', customPrimary);
   $mdThemingProvider.definePalette('domo-primary', customPrimaryPalette);
 
-  const customAccent =
-    {
-      'A200': '#72b0d7' // blue accent color for tabs
-    };
+  const customAccent = {
+    'A200': '#72b0d7' // blue accent color for tabs
+  };
   const customAccentPalette = $mdThemingProvider.extendPalette('blue', customAccent);
   $mdThemingProvider.definePalette('domo-accent', customAccentPalette);
 
-  const customWarn =
-    {
-      '600': '#fbad56', // orange pill
-      '700': '#e4584f' // red warn of little in-stock indicator
-    };
+  const customWarn = {
+    '600': '#fbad56', // orange pill
+    '700': '#e4584f' // red warn of little in-stock indicator
+  };
   const customWarnPalette = $mdThemingProvider.extendPalette('red', customWarn);
   $mdThemingProvider.definePalette('domo-warn', customWarnPalette);
 
